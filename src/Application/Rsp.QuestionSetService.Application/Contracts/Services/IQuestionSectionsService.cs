a9e169cba23013b6b4550e07e0749a0e411239f4
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-02-24 11:30:28 +0000

    Added Question Sections & Question Categories Controllers

---
+++ src/Application/Rsp.QuestionSetService.Application/Contracts/Services/IQuestionSectionsService.cs
@@ -0,0 +1,26 @@
+﻿using Rsp.Logging.Interceptors;
+using Rsp.QuestionSetService.Application.DTOS.Responses;
+
+namespace Rsp.QuestionSetService.Application.Contracts.Services;
+
+/// <summary>
+///     Question Section service interface. Marked as IInterceptable to enable
+///     the start/end logging for all methods.
+/// </summary>
+public interface IQuestionSectionsService : IInterceptable
+{
+    /// <summary>
+    ///     Gets all question sections
+    /// </summary>
+    Task<IEnumerable<QuestionSectionDto>> GetQuestionSections();
+
+    /// <summary>
+    ///     Gets the previous question sections
+    /// </summary>
+    Task<QuestionSectionDto> GetPreviousQuestionSections(string currentSectionId);
+
+    /// <summary>
+    ///     Gets the next question sections
+    /// </summary>
+    Task<QuestionSectionDto> GetNextQuestionSections(string currentSectionId);
+}
