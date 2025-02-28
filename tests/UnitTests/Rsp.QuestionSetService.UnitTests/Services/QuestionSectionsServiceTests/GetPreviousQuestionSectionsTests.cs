6148e3b236ecf17c5466571427bd18f0558ca4d5
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-02-28 10:05:36 +0000

    Added unit tests on new code

---
+++ tests/UnitTests/Rsp.QuestionSetService.UnitTests/Services/QuestionSectionsServiceTests/GetPreviousQuestionSectionsTests.cs
@@ -0,0 +1,42 @@
+﻿using Rsp.QuestionSetService.Application.Contracts.Repositories;
+using Rsp.QuestionSetService.Application.Specifications;
+using Rsp.QuestionSetService.Domain.Entities;
+using Rsp.QuestionSetService.Services;
+
+namespace Rsp.QuestionSetService.UnitTests.Services.QuestionSectionsServiceTests;
+
+public class GetPreviousQuestionSectionsTests : TestServiceBase<QuestionSectionsService>
+{
+    [Fact]
+    public async Task GetPreviousQuestionSections_ShouldReturnPreviousSection()
+    {
+        // Arrange
+        var sectionId = "2";
+        var previousSection = new QuestionSection { SectionId = "1", SectionName = "Previous Section" };
+
+        Mocker
+            .GetMock<IQuestionSectionsRepository>()
+            .Setup(repo =>
+                repo.GetPreviousQuestionSection(It.Is<QuestionSectionSpecification>(s => CheckSectionId(s, sectionId))))
+            .ReturnsAsync(previousSection);
+
+        // Act
+        var result = await Sut.GetPreviousQuestionSections(sectionId);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.SectionId.ShouldBe(previousSection.SectionId);
+        result.SectionName.ShouldBe(previousSection.SectionName);
+    }
+
+
+    // Helper function to validate that the sectionId was passed correctly
+    private bool CheckSectionId(QuestionSectionSpecification spec, string expectedSectionId)
+    {
+        // Create a new instance of the spec and see if it behaves the same way
+        var testSpec = new QuestionSectionSpecification(expectedSectionId);
+
+        // Compare the string representation of the LINQ expression (simpler than inspecting expressions directly)
+        return testSpec.ToString() == spec.ToString();
+    }
+}
