6049f60b96fe5736c1254e125afb615b66cca0c0
Haris Amin <haris.amin_PA@hra.nhs.uk>
2025-03-14 12:41:37 +0000

    RSP-1568/1570: Question set versioning and preview draft versions - Add test coverage

---
+++ tests/UnitTests/Rsp.QuestionSetService.UnitTests/Services/QuestionServiceTests/PublishVersionTests.cs
@@ -0,0 +1,38 @@
+﻿using Rsp.QuestionSetService.Application.Contracts.Repositories;
+using Rsp.QuestionSetService.Services;
+
+namespace Rsp.QuestionSetService.UnitTests.Services.QuestionServiceTests;
+
+public class PublishVersionTests : TestServiceBase<QuestionService>
+{
+    [Theory, AutoData]
+    public async Task PublishVersion_ShouldCallRepository(string versionId)
+    {
+        // Arrange
+        Mocker
+            .GetMock<IQuestionRepository>()
+            .Setup(repo => repo.PublishVersion(It.IsAny<string>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        await Sut.PublishVersion(versionId);
+
+        // Assert
+        Mocker
+            .GetMock<IQuestionRepository>()
+            .Verify(repo => repo.PublishVersion(It.IsAny<string>()), Times.Once);
+    }
+
+    [Theory, AutoData]
+    public async Task AddQuestion_ShouldHandleRepositoryException(string versionId)
+    {
+        // Arrange
+        Mocker
+            .GetMock<IQuestionRepository>()
+            .Setup(repo => repo.PublishVersion(It.IsAny<string>()))
+            .ThrowsAsync(new Exception("Repository error"));
+
+        // Act & Assert
+        await Should.ThrowAsync<Exception>(async () => await Sut.PublishVersion(versionId));
+    }
+}
